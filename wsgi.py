b5ab8ff3a60485bb678593efe90d78daf4e3b10d
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2011-08-20 14:30:21 +1000

    going redis

--- wsgi.py
+++ wsgi.py
@@ -1,24 +1,24 @@
+import json
+
 import cherrypy
 
 class Root:
 
     @cherrypy.expose
     def index(self):
-        return """
-<!DOCTYPE HTML>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>CherryPy on dotCloud : a match made in heaven</title>
-</head>
-<body>
-	<img src="static/cherrypy.png" alt="CherryPy" />
-    <img src="static/dotcloud.png" alt="dotCloud" />
-</body>
-</html>"""
+        counter = cherrypy.session.get('counter', 0)
+        cherrypy.session['counter'] = counter + 1
+        return counter
+
+with open('/home/dotcloud/environment.js') as f:
+    environment = json.load(f)
 
 config = {'/' :{
-    'tools.session.on' : True
+    'tools.session.on' : True,
+    'tools.session.storage_type' : 'redis',
+    'tools.session.host' : environment['DOTCLOUD_DATA_REDIS_HOST'],
+    'tools.session.port' : environment['DOTCLOUD_DATA_REDIS_PORT'],
+    'tools.session.password' : environment['DOTCLOUD_DATA_REDIS_PASSWORD']
 }}
 
 application = cherrypy.tree.mount(Root(), '', config=config)
