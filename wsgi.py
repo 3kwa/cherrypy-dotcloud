0ce42099466f0a8b3e8db1a50f0e806722757d1d
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2011-08-20 15:47:44 +1000

    comments and integer port -> cherrys

--- wsgi.py
+++ wsgi.py
@@ -1,26 +1,32 @@
 import json
 
 import cherrypy
-import cherrys
 
+# pluging in a Redis backend for CherryPy sessions
+# see http://pypi.python.org/pypi/cherrys
+import cherrys
 cherrypy.lib.sessions.RedisSession = cherrys.RedisSession
 
 class Root:
 
     @cherrypy.expose
     def index(self):
-        counter = cherrypy.session.get('counter', 0)
+        """ dummy controller counting the number of visits """
+        counter = cherrypy.session.get('counter', 1)
         cherrypy.session['counter'] = counter + 1
-        return "%i" % counter
+        return "visit : %i" % counter
 
+# getting the dotCloud environment variables
+# see http://docs.dotcloud.com/guides/environment/
 with open('/home/dotcloud/environment.json') as f:
     environment = json.load(f)
 
+# configuring CherryPy sessions
 config = {'/' :{
     'tools.sessions.on' : True,
     'tools.sessions.storage_type' : 'redis',
     'tools.sessions.host' : environment['DOTCLOUD_SESSION_REDIS_HOST'],
-    'tools.sessions.port' : int(environment['DOTCLOUD_SESSION_REDIS_PORT']),
+    'tools.sessions.port' : environment['DOTCLOUD_SESSION_REDIS_PORT'],
     'tools.sessions.password' : environment['DOTCLOUD_SESSION_REDIS_PASSWORD']
 }}
 
